50e8180170ccd09394d3870b07e7226a39e4cbb2
Mark Hogan <hoganmark2021@gmail.com>
2023-12-21 20:24:12 +0000

    Mongo Lab Exercises Done

--- node_lab1/tasky-api/api/users/userModel.js
+++ node_lab1/tasky-api/api/users/userModel.js
@@ -6,5 +6,10 @@ const UserSchema = new Schema({
   username: { type: String, unique: true, required: true},
   password: {type: String, required: true }
 });
+const passwordType = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/;
+const passwordValidator = (password) => {
+  return password !== "" && passwordType.test(password);
+};
+UserSchema.path("password").validate(passwordValidator);
 
 export default mongoose.model('User', UserSchema);
--- node_lab1/tasky-api/index.js
+++ node_lab1/tasky-api/index.js
@@ -3,6 +3,7 @@ import dotenv from 'dotenv';
 import express from 'express';
 import tasksRouter from './api/tasks';
 import usersRouter from './api/users';
+import cors from 'cors';
 
 dotenv.config();
 
@@ -19,10 +20,10 @@ const errHandler = (err, req, res, next) => {
 const app = express();
 
 const port = process.env.PORT;
-
+// Enable CORS for all requests
+app.use(cors());
 app.use(express.json());
 app.use('/api/tasks', tasksRouter);
-
 app.use(errHandler);
 app.use('/api/users', usersRouter);
 app.listen(port, () => {
